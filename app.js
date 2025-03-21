ae2072c7884fe409f525508aa750a01e697ab0b8
narendrachitteti <chittetinarendra2000@gmail.com>
2025-03-21 18:19:21 +0530

    cors

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ dotenv.config({ path: './config.env' });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require("cors");
 // Use CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:2000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: ["http://localhost:2000", "http://localhost:3000"],
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
+
 
 // Connect to MongoDB
 connectDB();
